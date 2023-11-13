f0f61388cf4518fdfa96802c87924930a8edc751
DDuijker <djoekeduijker@live.nl>
2023-11-13 17:34:01 +0100

    Finished pong game

--- 22.Pong/main.py
+++ 22.Pong/main.py
@@ -1,5 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 # Create the screen
@@ -8,11 +10,13 @@ screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.tracer(0)
 screen.title("Pong")
+KEYBINDINGS = [['w', 's'], ['Up', 'Down']]
+screen.listen()
 
 right_paddle = Paddle(x_position=350)
 left_paddle = Paddle(x_position=-350)
-KEYBINDINGS = [['w', 's'], ['Up', 'Down']]
-screen.listen()
+ball = Ball()
+scoreboard = Scoreboard()
 
 
 # For loop to enable keybinds
@@ -27,8 +31,26 @@ register_keybindings(right_paddle, KEYBINDINGS[1])
 game_is_on = True
 
 while game_is_on:
+    ball.move()
+    time.sleep(ball.moving_speed)
     screen.update()
-    time.sleep(0.1)
 
+    # Detect collision with a wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+        ball.speed_up()
+    # Detect collisions with paddles
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 320 or ball.distance(left_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+        ball.speed_up()
+    # Detect when the ball missed the paddle
+    if ball.xcor() > 390:
+        # Right
+        ball.ball_reset()
+        scoreboard.increase_score_left()
+    if ball.xcor() < -390:
+        # Left
+        ball.ball_reset()
+        scoreboard.increase_score_right()
 
 screen.exitonclick()
--- 22.Pong/scoreboard.py
+++ 22.Pong/scoreboard.py
@@ -1,9 +1,36 @@
 from turtle import Turtle
 
+ALIGN = 'center'
+FONT = ('Courier', 80, 'normal')
+LEFT_POSITION = (-100, 200)
+RIGHT_POSITION = (100, 200)
+
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.left_score = 0
+        self.right_score = 0
+        self.update_scoreboard()
 
+    def increase_score_right(self):
+        self.clear()
+        self.right_score += 1
+        self.update_scoreboard()
 
+    def increase_score_left(self):
+        self.clear()
+        self.left_score += 1
+        self.update_scoreboard()
+
+    def write_score(self, score, position):
+        self.goto(position)
+        self.write(score, align=ALIGN, font=FONT)
+
+    def update_scoreboard(self):
+        self.write_score(self.left_score, LEFT_POSITION)
+        self.write_score(self.right_score, RIGHT_POSITION)
 
